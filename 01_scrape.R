efd40ce98d45b8f966bdb521ffb83bc041e2bd2f
smsarkar1994 <60992047+smsarkar1994@users.noreply.github.com>
2022-02-22 14:40:48 -0500

    Update 01_scrape.R

--- 01_scrape.R
+++ 01_scrape.R
@@ -1,20 +1,38 @@
 rm(list = ls())
 library(tidyverse)
 library(rvest)
+library(data.table)
 
-dir <- "C:/Users/samee/Documents/oneoffs/11_pokemon_scrape/"
+dir <- #insert directory here
 input <- paste0(dir, "input/")
 output <- paste0(dir, "output/")
 
+gen_data <- function(move_type, gen) {
+  
+  url <- glue::glue("https://pokemondb.net/move/generation/{gen}#cat={move_type}")
+  
+  df <- read_html(url) %>%
+    html_table() %>%
+    data.frame() %>%
+    mutate(move_type = move_type,
+           gen_introduced = gen)
+  
+  return(df)
+}
 
+test <- gen_data("physical", 1)
 
-url <- glue::glue("https://pokemondb.net/move/generation/8")
+all_moves <- data.frame()
 
+for (i in 1:8) {
+  for (j in c("physical", "special", "status")) {
+    tmp <- gen_data(j, i)
+    l <- list(all_moves, tmp)
+    all_moves <- rbindlist(l)
+  }
+}
 
+all_moves <- all_moves %>%
+  dplyr::select(-`Cat.`)
 
-df <- read_html(url) %>%
-  html_table() %>%
-  data.frame() 
-
-
-write.csv(df, paste0(output, "01_moves_gen_8.csv"))
+write.csv(all_moves, paste0(output, "01_moves_gen_8.csv"))
