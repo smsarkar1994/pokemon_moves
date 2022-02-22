42938b2ad53f38ffd2bd5324e18612c3ff2b2e46
smsarkar1994 <60992047+smsarkar1994@users.noreply.github.com>
2022-02-22 15:10:36 -0500

    Update 01_scrape.R

--- 01_scrape.R
+++ 01_scrape.R
@@ -3,36 +3,72 @@ library(tidyverse)
 library(rvest)
 library(data.table)
 
-dir <- #insert directory here
+dir <- "C:/Users/samee/Documents/oneoffs/11_pokemon_scrape/"
 input <- paste0(dir, "input/")
 output <- paste0(dir, "output/")
 
-gen_data <- function(move_type, gen) {
+
+gen_data <- function(move_type) {
   
-  url <- glue::glue("https://pokemondb.net/move/generation/{gen}#cat={move_type}")
+  url <- glue::glue("https://www.serebii.net/attackdex-swsh/{move_type}.shtml")
   
   df <- read_html(url) %>%
-    html_table() %>%
-    data.frame() %>%
-    mutate(move_type = move_type,
-           gen_introduced = gen)
+    html_table() 
+  
+  df <- df[2] %>%
+    data.frame() 
+  
+  colnames(df) <- df[1,]
+  df <- df[-1,]
+  
+  df <- df %>%
+    mutate(move_type = move_type)
   
   return(df)
 }
 
-test <- gen_data("physical", 1)
 
 all_moves <- data.frame()
 
-for (i in 1:8) {
-  for (j in c("physical", "special", "status")) {
-    tmp <- gen_data(j, i)
-    l <- list(all_moves, tmp)
-    all_moves <- rbindlist(l)
-  }
+for (j in c("physical", "special", "other")) {
+  tmp <- gen_data(j)
+  l <- list(all_moves, tmp)
+  all_moves <- rbindlist(l)
 }
 
-all_moves <- all_moves %>%
-  dplyr::select(-`Cat.`)
 
-write.csv(all_moves, paste0(output, "01_moves_gen_8.csv"))
+write.csv(all_moves, paste0(output, "01_moves_sw_sh.csv"))
+
+
+#Example code for getting pokemon level data from a move
+tmp_move <- tolower(all_moves[10, "Name"])
+
+url <- glue::glue("https://www.serebii.net/attackdex-swsh/{tmp_move}.shtml")
+df <- read_html(url) %>%
+  html_table()
+keep <- grep("Base Stats", df)
+df <- df[keep]
+
+clean_df <- function(df) {
+  df <- data.frame(df)
+  colnames(df) <- df[2,]
+  df <- df[-c(1:2),]
+  df <- repair_names(df)
+
+  df <- df %>%
+    filter(!is.na(Name)) %>%
+    dplyr::select(`No.`, Type) 
+  
+}
+
+
+name_pokemon <- data.frame() 
+
+for(i in 1:length(df)) {
+  tmp <- clean_df(df[i])
+  l <- list(name_pokemon, tmp)
+  name_pokemon <- rbindlist(l)
+}
+name_pokemon <- name_pokemon %>%
+  mutate(move = tmp_move)
+
